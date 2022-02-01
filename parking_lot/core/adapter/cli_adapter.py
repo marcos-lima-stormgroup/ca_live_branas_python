8d125ec016c1a760f94fa464fe77aca7598c27b2
Marcos Lima <marcos.j.s.lima@gmail.com>
2022-02-01 13:53:32 -0300

    Implementado CLI App/adapter

---
+++ parking_lot/core/adapter/cli_adapter.py
@@ -0,0 +1,12 @@
+class CliAdapter:
+    @staticmethod
+    def handle_get(controller):
+        def handle(code):
+            response = controller(code)
+            return {"code": response.code,
+                    "capacity": response.capacity,
+                    "open_hour": response.open_hour,
+                    "close_hour": response.close_hour,
+                    "occupied_spaces": response.occupied_spaces}
+
+        return handle
---
+++ parking_lot/infra/CLI/cli_app.py
@@ -0,0 +1,22 @@
+import sys
+
+from parking_lot.core.adapter.cli_adapter import CliAdapter
+from parking_lot.core.controller.parking_lot_controller import \
+    ParkingLotController
+from parking_lot.core.interactors.get_parking_lot import \
+    GetParkingLotRequestModel
+
+if __name__ == '__main__':
+    print('CLI Parking Lot')
+    try:
+        if sys.argv[1] == 'get_parking_lot':
+            code = sys.argv[2]
+            get_request = GetParkingLotRequestModel(code)
+            response = CliAdapter.handle_get(
+                ParkingLotController.get_parking_lot)(get_request)
+
+            print(response)
+    except IndexError as e:
+        print('Passe parametros!')
+
+    print('Done')
